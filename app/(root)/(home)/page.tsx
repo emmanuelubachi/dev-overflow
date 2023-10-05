7c32889cf381c187a9b780c0dc80e53054db96ca
Emmanuel Ubachi <emmanuelubachi@gmail.com>
2023-10-05 05:34:02 +0400

    add filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filters from "@/components/shared/Filters";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 export default function Home() {
@@ -30,8 +33,13 @@ export default function Home() {
           placeholder="Search for questions"
           otherClasses="flex-1"
         />
-        Filters
+        <Filters
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
+      <HomeFilters />
     </>
   );
 }
